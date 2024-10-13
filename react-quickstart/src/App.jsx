c08933ef900a8fb35f7dd373bbeebf06977e9fe7
ElengPregunts28 <ellainepregunta28@gmail.com>
2024-10-14 07:32:40 +0800

    Im done with displaying data.

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,7 +1,24 @@
 import './App.css';
 
-function Head() {
-  return <img className="avatar" />;
-}
+const user = {
+  name: 'Cutie Pie',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 400,
+};
 
-export default Head;
+export default function Profile() {
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img 
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style = {{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </>
+  );
+}
