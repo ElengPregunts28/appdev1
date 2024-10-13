d6ad1538541188baa82ea8c1840a9d5d46d4deab
ElengPregunts28 <ellainepregunta28@gmail.com>
2024-10-13 19:01:47 +0800

    I'm done writing markup with JSX.

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,14 +1,10 @@
-function MyButton() {
-  return (
-    <button>I'm a button.</button>
+function AboutPage() {
+  return(
+    <>
+      <h1>About</h1>
+      <p>Hello there!<br />How do you do?</p>
+    </>
   );
 }
 
-export default function MyApp() {
-  return (
-    <div>
-      <h1>Welcome to my App!</h1>
-      <MyButton />
-    </div>
-  );
-}
+export default AboutPage
