b2df7c6ea2577bb4a5b7c84d63b2a3bfe2327495
ElengPregunts28 <ellainepregunta28@gmail.com>
2024-11-16 14:14:43 +0800

    Done in Part 1: useState 3. Form Input.

--- react-hooks/src/main.jsx
+++ react-hooks/src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import ToggleButton from './ToggleButton.jsx'
+import TextInput from './TextInput.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ToggleButton />
+    <TextInput />
   </StrictMode>,
 )
